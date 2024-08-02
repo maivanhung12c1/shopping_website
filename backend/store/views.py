3b733bc124396735f505ee02460378ea7092496e
maivanhung <maivanhung.20119@gmail.com>
2024-08-03 00:07:56 +0700

    Update store app

--- backend/store/views.py
+++ backend/store/views.py
@@ -77,4 +77,290 @@ class ProductDetailView(generics.RetrieveAPIView):
     def get_object(self):
         slug = self.kwargs.get('slug')
         return Product.objects.get(slug=slug)
+    
+class CartApiView(generics.ListCreateAPIView):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.all()
+    permission_classes = (AllowAny,)
 
+    def create(self, request, *args, **kwargs):
+        payload = request.data
+
+        product_id = payload['product']
+        user_id = payload['user_id']
+        qty = payload['qty']
+        price = payload['price']
+        shipping_amount = payload['shipping_amount']
+        country = payload['country']
+        size = payload['size']
+        color = payload['color']
+        cart_id = payload['cart_id']
+
+        product = Product.objects.filter(status="published", id=product_id).first()
+        if user_id != "undefined":
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = None
+        
+        tax = Tax.objects.filter(country=country).first()
+        if tax:
+            tax_rate = tax.rate / 100
+        else:
+            tax_rate = 0
+        
+        cart = Cart.objects.filter(cart_id=cart_id, product=product).first()
+        if cart:
+            cart.product = product
+            cart.user = user
+            cart.qty = qty
+            cart.price = price
+            cart.sub_total = Decimal(price) * int(qty)
+            cart.shipping_amount = Decimal(shipping_amount) * int(qty)
+            cart.size = size
+            cart.tax_fee = int(qty) * Decimal(tax_rate)
+            cart.color = color
+            cart.country = country
+            cart.card_id = cart_id
+
+            config_settings = ConfigSettings.objects.first()
+
+            if config_settings.service_fee_charge_type == "percentage":
+                service_fee_percentage = config_settings.service_fee_percentage / 100
+                cart.service_fee = Decimal(service_fee_percentage) * cart.sub_total
+            else:
+                cart.service_fee = config_settings.service_fee_flat_rate
+
+            cart.total = cart.sub_total + cart.shipping_amount + cart.service_fee + cart.tax_fee
+            cart.save()
+
+            return Response({"message": "Cart updated successfully"}, status=status.HTTP_200_OK)
+        else:
+            cart = Cart()
+            cart.product = product()
+            cart.user = user
+            cart.qty = qty
+            cart.price = price
+            cart.sub_total = Decimal(price) * int(qty)
+            cart.shipping_amount = Decimal(shipping_amount) * int(qty)
+            cart.size = size
+            cart.tax_fee = int(qty) * Decimal(tax_rate)
+            cart.color = color
+            cart.country = country
+            cart.card_id = cart_id
+
+            config_settings = ConfigSettings.objects.first()
+
+            if config_settings.service_fee_charge_type == "percentage":
+                service_fee_percentage = config_settings.service_fee_percentage / 100
+                cart.service_fee = Decimal(service_fee_percentage) * cart.sub_total
+            else:
+                cart.service_fee = config_settings.service_fee_flat_rate
+
+            cart.total = cart.sub_total + cart.shipping_amount + cart.service_fee + cart.tax_fee
+            cart.save()
+
+            return Response({"message": "Cart created successfully"}, status=status.HTTP_200_OK)
+        
+class CartListView(generics.ListAPIView):
+    serializer_class = CartSerializer
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        cart_id = self.kwargs['cart_id']
+        user_id = self.kwargs.get('user_id') # Use get() method to handler the case where user_id is not present
+
+        if user_id is not None:
+            user = User.objects.get(id=user_id)
+            queryset = Cart.objects.filter(Q(user=user, cart_id=cart_id) | Q(user=user))
+        else:
+            queryset = Cart.objects.filter(cart_id=cart_id)
+        return queryset
+    
+class CartTotalView(generics.ListAPIView):
+    serializer_class = CartSerializer
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        cart_id = self.kwargs['cart_id']
+        user_id = self.kwargs.get('user_id')
+
+        if user_id is not None:
+            user = User.objects.get(id=user_id)
+            queryset = Cart.objects.filter(cart_id=cart_id, user=user)
+        else:
+            queryset = Cart.objects.filter(cart_id=cart_id)
+        
+        return queryset
+
+class CartDetailView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    # Specify the lookup field for retrieving objects using 'cart_id'
+    lookup_field = 'cart_id'
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        cart_id = self.kwargs['cart_id']
+        user_id = self.kwargs.get('user_id')
+
+        if user_id is not None:
+            user = User.objects.get(id=user_id)
+            queryset = Cart.objects.filter(cart_id=cart_id, user=user)
+        else:
+            queryset = Cart.objects.filter(cart_id=cart_id)
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        # Get the queryset of cart items based on cart_id and user_id (if provided)
+        queryset = self.get_queryset()
+
+        # Initialize sums for various cart item attributes
+        total_shipping = 0.0
+        total_tax = 0.0
+        total_service_fee = 0.0
+        total_sub_total = 0.0
+        total_total = 0.0
+
+        # Iterate over the queryset of cart items to calculate cumulative sums
+        for cart_item in queryset:
+            # Calculate the cumulative shipping, tax, service_fee, and total values
+            total_shipping += float(self.calculate_shipping(cart_item))
+            total_tax += float(self.calculate_tax(cart_item))
+            total_service_fee += float(self.calculate_service_fee(cart_item))
+            total_sub_total += float(self.calculate_sub_total(cart_item))
+            total_toal += round(float(self.calculate_total(cart_item)), 2)
+        
+        # Create a data dict to store the cumulative values
+        data = {
+            'shipping': round(total_shipping, 2),
+            'tax': total_tax,
+            'service_fee': total_service_fee,
+            'sub_total': total_sub_total,
+            'total': total_total,
+        }
+
+        return Response(data)
+    
+    def calculate_shipping(self, cart_item):
+        return cart_item.shipping_amount
+    
+    def calculate_tax(self, cart_item):
+        return cart_item.tax_fee
+    
+    def calculate_service_fee(self, cart_item):
+        return cart_item.service_fee
+    
+    def calculate_sub_total(self, cart_item):
+        return cart_item.sub_total
+    
+    def calculate_total(self, cart_item):
+        return cart_item.total
+    
+class CartItemDeleteView(generics.DestroyAPIView):
+    serializer_class = CartSerializer
+    lookup_field = 'cart_id'
+
+    def get_object(self):
+        cart_id = self.kwargs['cart_id']
+        item_id = self.kwargs['item_id']
+        user_id = self.kwargs.get('user_id')
+
+        if user_id is not None:
+            user = get_list_or_404(User, id=user_id)
+            cart = get_list_or_404(Cart, cart_id=cart_id, id=item_id, user=user)
+        else:
+            cart = get_list_or_404(Cart, cart_id=cart_id, id=item_id)
+
+        return cart
+    
+class CreateOrderView(generics.CreateAPIView):
+    serializer_class = CartOrderSerializer
+    queryset = CartOrder.objects.all()
+    permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        payload = request.data
+
+        full_name = payload['full_name']
+        email = payload['email']
+        mobile = payload['mobile']
+        address = payload['address']
+        city = payload['city']
+        state = payload['state']
+        country = payload['country']
+        cart_id = payload['cart_id']
+        user_id = payload['user_id']
+
+        if user_id != 0:
+            user = User.objects.filter(id=user_id).first()
+        else:
+            None
+        
+        cart_items = Cart.objects.filter(cart_id=cart_id)
+        total_shipping = Decimal(0.0)
+        total_tax = Decimal(0.0)
+        total_service_fee = Decimal(0.0)
+        total_sub_total = Decimal(0.0)
+        total_initial_total = Decimal(0.0)
+        total_total = Decimal(0.0)
+
+        with transaction.atomic():
+
+            order = CartOrder.objects.create(
+                buyer=user,
+                payment_status="processing",
+                full_name=full_name,
+                email=email,
+                mobile=mobile,
+                address=address,
+                city=city,
+                state=state,
+                country=country
+            )
+
+            for c in cart_items:
+                CartOrderItem.objects.create(
+                    order=order,
+                    product=c.product,
+                    qty=c.qty,
+                    color=c.color,
+                    size=c.size,
+                    price=c.price,
+                    sub_total=c.sub_total,
+                    shipping_amount=c.shipping_amount,
+                    tax_fee=c.tax_fee,
+                    service_fee=c.service_fee,
+                    total=c.total,
+                    initial_total=c.total,
+                    vendor=c.product.vendor
+                )
+
+                total_shipping += Decimal(c.shipping_amount)
+                total_tax += Decimal(c.tax_fee)
+                total_service_fee += Decimal(c.service_fee)
+                total_sub_total += Decimal(c.sub_total)
+                total_initial_total += Decimal(c.total)
+                total_total += Decimal(c.total)
+
+                order.vendor.add(c.product.vendor)
+
+            order.sub_total = total_sub_total
+            order.shipping_amount = total_shipping
+            order.tax_fee = total_tax
+            order.service_fee = total_service_fee
+            order.initial_total = total_initial_total
+            order.total = total_total
+
+            order.save()
+
+        return Response({'message': 'Order created susscessfully', 'order_id': order.oid}, status=status.HTTP_201_CREATED)
+    
+
+class CheckoutView(generics.RetrieveAPIView):
+    serializer_class = CartOrderSerializer
+    lookup_field = 'order_oid'
+
+    def get_object(self):
+        order_oid = self.kwargs['order_oid']
+        cart = get_list_or_404(CartOrder, oid=order_oid)
+        return cart
+    
