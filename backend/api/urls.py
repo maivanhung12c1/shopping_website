f5b65b41d584d28ab8fc9822e3e2f94354c274cd
maivanhung <maivanhung.20119@gmail.com>
2024-09-19 23:21:53 +0700

    update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -54,6 +54,37 @@ urlpatterns = [
     path('customer/setting/<int:pk>/', customer_views.CustomerUpdateView.as_view(), name='customer-settings'),
 
     # Vendor API Endpoints
-    path('vendor-register/', vendor_views.VendorRegister.as_view(), name='vendor-register')
+    path('vendor/stats/<vendor_id>/', vendor_views.DashboardStatsAPIView.as_view(), name='vendor-stats'),
+    path('vendor/products/<vendor_id>/', vendor_views.ProductsAPIView.as_view(), name='vendor-products'),
+    path('vendor/orders/<vendor_id>/', vendor_views.OrdersAPIView.as_view(), name='vendor-orders'),
+    path('vendor/orders/<vendor_id>/<order_oid>/', vendor_views.OrderDetailAPIView.as_view(), name='vendor-order-detail'),
+    path('vendor/yearly-report/<vendor_id>/', vendor_views.YearlyOrderReportChartAPIView.as_view(), name='vendor-yearly-report'),
+    path('vendor-orders-report-chart/<vendor_id>', vendor_views.MonthlyOrderChartAPIFBV, name='vendor-orders-report-chart'),
+    path('vendor-products-report-chart/<vendor_id>/', vendor_views.MonthlyProductsChartAPIFBV, name='vendor-products-report-chart'),
+    path('vendor-product-create/<vendor_id>/', vendor_views.ProductCreateView.as_view(), name='vendor-product-create'),
+    path('vendor-product-edit/<vendor_id>/<product_pid>/', vendor_views.ProductUpdateAPIView.as_view(), name='vendor-product-edit'),
+    path('vendor-product-delete/<vendor_id>/<product_pid>/', vendor_views.ProductDeleteAPIView.as_view(), name='vendor-product-delete'),
+    path('vendor-product-filter/<vendor_id>/', vendor_views.FilterProductsAPIView.as_view(), name='vendor-product-filter'),
+    path('vendor-earning/<vendor_id>/', vendor_views.Earing.as_view(), name='vendor-earning'),
+    path('vendor-monthly-earning/<vendor_id>/', vendor_views.MonthlyEarningTracker, name='vendor-earning-monthly'),
+    path('vendor-reviews/<vendor_id>/', vendor_views.ReviewsListAPIView.as_view(), name='vendor-reviews'),
+    path('vendor-reviews/<vendor_id>/<review_id>', vendor_views.ReviewsDetailAPIView.as_view(), name='vendor-review-detail'),
+    path('vendor-coupon-list/<vendor_id>', vendor_views.CouponListAPIView.as_view(), name='vendor-coupon-list'),
+    path('vendor-coupon-stats/<vendor_id>/', vendor_views.CouponStats.as_view(), name='vendor-coupon-stats'),
+    path('vendor-coupon-detail/<vendor_id>/<coupon_id>/', vendor_views.CouponDetailAPIView.as_view(), name='vendor-coupon-detail'),
+    path('vendor-coupon-create/<vendor_id>/', vendor_views.CouponCreateAPIView.as_view(), name='vendor-coupon-create'),
+    path('vendor-notifications-unseen/<vendor_id>/', vendor_views.NotificationUnSeenListAPIView.as_view(), name='vendor-noti-unseen'),
+    path('vendor-notifications-seen/<vendor_id>/', vendor_views.NotificationSeenListAPIView.as_view(), name='vendor-noti-seen'),
+    path('vendor-notifications-summary/<vendor_id>/', vendor_views.NotificationSummaryAPIView.as_view(), name='vendor-noti-summary'),
+    path('vendor-notifications-mark-as-seen/<vendor_id>/<noti_id>/', vendor_views.NotificationMarkAsSeen.as_view(), name='vendor-noti-mark-seen'),
+    path('vendor-settings/<int:pk>/', vendor_views.VendorProfileUpdateView.as_view(), name='vendor-settings'),
+    path('vendor-shop-settings/<int:pk>/', vendor_views.ShopUpdateView.as_view(), name='shop-settings'),
+    path('shop/<vendor_slug>/', vendor_views.ShopAPIView.as_view(), name='shop'),
+    path('vendor-products/<vendor_slug>/', vendor_views.ShopProductsAPIView.as_view(), name='vendor-products'),
+    path('vendor-register/', vendor_views.VendorRegister.as_view(), name='vendor-register'),
 
+    # Tracking Feature
+    path('vendor/couriers/', vendor_views.CourierListAPIView.as_view()),
+    path('vendor/order-item-detail/<int:pk>/', vendor_views.OrderItemDetailAPIView.as_view())
+    
 ]
---
+++ frontend/src/views/vendor/AddProduct.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import apiInstance from '../../utils/axios';
+import UserData from '../plugin/UserData';
+
+
+function AddProduct() {
+  return (
+    <div>AddProduct</div>
+  )
+}
+
+export default AddProduct
